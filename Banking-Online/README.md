e095a4a4bef7f5bfe85059f88e32850bb42fa95a
Punit Prem Gautam <punit.gautam@irissoftware.com>
2019-06-21 14:41:58 +0530

    Initial commit

---
+++ Banking-Online/README.md
@@ -0,0 +1,18 @@
+# Online-Bank
+Spring Boot/Spring Data/Spring Security/Hibernate/MySQL/REST
+
+The project online banking system. It allows to register/login, deposit/withdraw money from accounts, add/edit recipients,
+transfer money between accounts and recipients, view transactions, make appointments.
+
+There are two roles user and admin. 
+
+The admin has there own fronent implemented in Angular2, which communicates with backend through REST services.
+
+
+There is a sql dump with prepopulated data inside project folder. 
+The username and password for database: root
+
+You can login with:
+User - password;
+Admin - password
+
---
+++ Banking-Online/src/main/java/com/userfront/controller/TransferController.java
@@ -0,0 +1,128 @@
+package com.userfront.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.Recipient;
+import com.userfront.domain.SavingsAccount;
+import com.userfront.domain.User;
+import com.userfront.service.TransactionService;
+import com.userfront.service.UserService;
+
+@Controller
+@RequestMapping("/transfer")
+public class TransferController {
+	
+	private static final String RECIPIENTLIST = "recipientList";
+	private static final String RECIPIENT = "recipient";
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/betweenAccounts")
+    public String betweenAccounts(Model model) {
+        model.addAttribute("transferFrom", "");
+        model.addAttribute("transferTo", "");
+        model.addAttribute("amount", "");
+
+        return "betweenAccounts";
+    }
+
+    @PostMapping(value = "/betweenAccounts")
+    public String betweenAccountsPost(
+            @ModelAttribute("transferFrom") String transferFrom,
+            @ModelAttribute("transferTo") String transferTo,
+            @ModelAttribute("amount") String amount,
+            Principal principal
+    ) throws Exception {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+        transactionService.betweenAccountsTransfer(transferFrom, transferTo, amount, primaryAccount, savingsAccount);
+
+        return "redirect:/userFront";
+    }
+    
+    @GetMapping(value = "/recipient")
+    public String recipient(Model model, Principal principal) {
+        List<Recipient> recipientList = transactionService.findRecipientList(principal);
+
+        Recipient recipient = new Recipient();
+
+        model.addAttribute(RECIPIENTLIST, recipientList);
+        model.addAttribute(RECIPIENT, recipient);
+
+        return RECIPIENT;
+    }
+
+    @PostMapping(value = "/recipient/save")
+    public String recipientPost(@ModelAttribute("recipient") Recipient recipient, Principal principal) { //NOSONAR
+
+        User user = userService.findByUsername(principal.getName());
+        recipient.setUser(user);
+        transactionService.saveRecipient(recipient);
+
+        return "redirect:/transfer/recipient";
+    }
+
+    @GetMapping(value = "/recipient/edit")
+    public String recipientEdit(@RequestParam(value = "recipientName") String recipientName, Model model, Principal principal){
+
+        Recipient recipient = transactionService.findRecipientByName(recipientName);
+        List<Recipient> recipientList = transactionService.findRecipientList(principal);
+
+        model.addAttribute(RECIPIENTLIST, recipientList);
+        model.addAttribute(RECIPIENT, recipient);
+
+        return RECIPIENT;
+    }
+
+    @GetMapping(value = "/recipient/delete")
+    @Transactional
+    public String recipientDelete(@RequestParam(value = "recipientName") String recipientName, Model model, Principal principal){
+
+        transactionService.deleteRecipientByName(recipientName);
+
+        List<Recipient> recipientList = transactionService.findRecipientList(principal);
+
+        Recipient recipient = new Recipient();
+        model.addAttribute(RECIPIENT, recipient);
+        model.addAttribute(RECIPIENTLIST, recipientList);
+
+
+        return RECIPIENT;
+    }
+
+    @GetMapping(value = "/toSomeoneElse")
+    public String toSomeoneElse(Model model, Principal principal) {
+        List<Recipient> recipientList = transactionService.findRecipientList(principal);
+
+        model.addAttribute(RECIPIENTLIST, recipientList);
+        model.addAttribute("accountType", "");
+
+        return "toSomeoneElse";
+    }
+
+    @PostMapping(value = "/toSomeoneElse")
+    public String toSomeoneElsePost(@ModelAttribute("recipientName") String recipientName, @ModelAttribute("accountType") String accountType, @ModelAttribute("amount") String amount, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        Recipient recipient = transactionService.findRecipientByName(recipientName);
+        transactionService.toSomeoneElseTransfer(recipient, accountType, amount, user.getPrimaryAccount(), user.getSavingsAccount());
+
+        return "redirect:/userFront";
+    }
+}
